4b380f66c7acdbadbb90d8d1d500de60301adde8
Marine Lasbleis <marine.lasbleis@gmail.com>
2018-08-19 19:56:14 +0900

    compaction columns for various velocity functions and delta/K.

--- compaction.py
+++ compaction.py
@@ -9,7 +9,7 @@ from mush import *
 
 
 def compaction_column(calcul_velocity, **options):
-    """ Compaction of a column of sediments. No velocity up or down.
+    """ Compaction of a column of sediments.
 
     Calcul_Velocity is a function (velocity_Sramek or velocity_Sumita) """
 
@@ -30,76 +30,9 @@ def compaction_column(calcul_velocity, **options):
     time = 0.
     dt_print = 2.
     time_p = time
-    time_max = 4000.
-    it = 0
-    iter_max = 5000
-
-    while time < time_max and it < iter_max:
-        # for it in range(0,10000):
-        it = it + 1
-        time = time + dt
-        time_p = time_p + dt
-        psi = update(velocity, psi, dt, R, options)
-        velocity = calcul_velocity(1 - psi, R, options)
-        v_m = np.amax(np.abs(velocity))
-        dt = min(0.5, 0.1 * dr / (v_m))
-        # if time_p > dt_print:
-        if it % 1000 == 0:
-            print(it, dt, time)
-            # reinitinalize the mark to know if we need to print/plot
-            # something.
-            time_p = time_p - dt_print
-            ax[0].plot(1 - psi, R[:-1] + dr / 2.)
-            ax[1].plot(velocity, R[1:-1])
-    print(it)
-    ax[0].set_xlim([0, 1])
-    ax[0].set_ylim([0, 1])
-    ax[0].set_xlabel("Porosity")
-    ax[0].set_ylabel("Height (non-dim)")
-    ax[1].set_xlabel("Solid velocity (non-dim)")
-
-
-
-def compaction_column_dVdz():
-    """ Compaction of a column of sediments.
-
-    Liquids can go through top (dVdz =0). Top boundary set at phi0.
-    Calcul_Velocity is a function (velocity_Sramek or velocity_Sumita) """
-
-    calcul_velocity = velocity_Sumita
-    options = {'advection': "upwind",
-               'Ra': 0.,
-               'eta': 1.,
-               'bc': '',
-               'phi0': 0.,
-               'phiN': 0.6,
-               'sign': 1,
-               'BC': "dVdz==0",
-               'coordinates': "cartesian"}
-    psi0 = 0.6 #1 - options["phi0"]
-    N = 1000
-    R = np.linspace(0, 1, N + 1)
-    dr = R[1] - R[0]
-    psi = psi0 * np.ones(N)
-
-    velocity = calcul_velocity(1 - psi, R, options)
-    v_m = np.amax(np.abs(velocity))
-    dt = min(0.5 * dr / (v_m), 0.5)
-    print(dt)
-
-    fig, ax = plt.subplots(1, 2, sharey=True)
-    fig2, ax2 = plt.subplots(2)
-    ax[0].plot(1 - psi, R[:-1] + dr / 2.)
-    ax[1].plot(velocity, R[1:-1])
-
-    time = 0.
-    dt_print = 2.
-    time_p = time
-    time_max = 4000.
+    time_max = 50.
     it = 0
     iter_max = 10000
-    ax2[0].plot(time, sum_phi(1-psi), '+')
-    ax2[1].plot(time, flux_top(1-psi, velocity), '+')
 
     while time < time_max and it < iter_max:
         # for it in range(0,10000):
@@ -110,25 +43,24 @@ def compaction_column_dVdz():
         velocity = calcul_velocity(1 - psi, R, options)
         v_m = np.amax(np.abs(velocity))
         dt = min(0.5, 0.1 * dr / (v_m))
-        ax2[0].plot(time, sum_phi(1-psi), '+')
-        ax2[1].plot(time, flux_top(1-psi, velocity), '+')
-        # if time_p > dt_print:
-        if it % 1000 == 0:
+        if time_p > dt_print:
+        # if it % 1000 == 0:
             print(it, dt, time)
             # reinitinalize the mark to know if we need to print/plot
             # something.
             time_p = time_p - dt_print
             ax[0].plot(1 - psi, R[:-1] + dr / 2.)
             ax[1].plot(velocity, R[1:-1])
-    print(it)
     ax[0].set_xlim([0, 1])
     ax[0].set_ylim([0, 1])
     ax[0].set_xlabel("Porosity")
     ax[0].set_ylabel("Height (non-dim)")
     ax[1].set_xlabel("Solid velocity (non-dim)")
+
 
 def sum_phi(phi):
     return np.sum(phi)/len(phi) #in cartesian
+
 def flux_top(phi, velocity):
     return (1-phi[-1])*velocity[-1]
 
@@ -172,64 +104,74 @@ def analytic_Sramek_spher(phi0, R, options):
 
 
 def figures_compaction_only():
-    options = {'advection': "upwind",
-               'Ra': 0.,
+    options = {'advection': "FLS",
                'eta': 1.,
-               'bc': '',
                'phi0': 0.,
                'phiN': 1.,
+               'phi_init': 0.3,
                'U0': 0.,
                'UN': 0.,
-               'sign': -1,
+               'sign': 1,
                'BC': "V==0",
                'coordinates': "cartesian"}
 
+    # top boundary impermeable
     compaction_column(velocity_Sramek, delta=1., **options)
     plt.savefig("fig/phi03_Sramek_delta_1.pdf")
     compaction_column(velocity_Sramek, delta=.5, **options)
     plt.savefig("fig/phi03_Sramek_delta_05.pdf")
-    compaction_column(velocity_Sramek, delta=.2, **options)
-    plt.savefig("fig/phi03_Sramek_delta_02.pdf")
     compaction_column(velocity_Sramek, delta=.1, **options)
     plt.savefig("fig/phi03_Sramek_delta_01.pdf")
-    compaction_column(velocity_Sramek, delta=.05, **options)
-    plt.savefig("fig/phi03_Sramek_delta_005.pdf")
 
     compaction_column(velocity_Sumita, K=1., **options)
     plt.savefig("fig/phi03_Sumita_K_1.pdf")
     compaction_column(velocity_Sumita, K=.5, **options)
     plt.savefig("fig/phi03_Sumita_K_05.pdf")
-    compaction_column(velocity_Sumita, K=.2, **options)
-    plt.savefig("fig/phi03_Sumita_K_02.pdf")
-    compaction_column(velocity_Sumita, K=.1, **options)
-    plt.savefig("fig/phi03_Sumita_K_01.pdf")
-    compaction_column(velocity_Sumita, K=.05, **options)
-    plt.savefig("fig/phi03_Sumita_K_005.pdf")
+
 
     options["coordinates"] = "spherical"
     compaction_column(velocity_Sramek, delta=1., **options)
     plt.savefig("fig/phi03_Sramek_delta_1_sph.pdf")
     compaction_column(velocity_Sramek, delta=.5, **options)
     plt.savefig("fig/phi03_Sramek_delta_05_sph.pdf")
-    compaction_column(velocity_Sramek, delta=.2, **options)
-    plt.savefig("fig/phi03_Sramek_delta_02_sph.pdf")
     compaction_column(velocity_Sramek, delta=.1, **options)
     plt.savefig("fig/phi03_Sramek_delta_01_sph.pdf")
-    compaction_column(velocity_Sramek, delta=.05, **options)
-    plt.savefig("fig/phi03_Sramek_delta_005_sph.pdf")
 
-    compaction_column(velocity_Sumita_spher, K=1., **options)
+
+    compaction_column(velocity_Sumita, K=1., **options)
     plt.savefig("fig/phi03_Sumita_K_1_sph.pdf")
-    compaction_column(velocity_Sumita_spher, K=.5, **options)
+    compaction_column(velocity_Sumita, K=.5, **options)
     plt.savefig("fig/phi03_Sumita_K_05_sph.pdf")
-    compaction_column(velocity_Sumita_spher, K=.2, **options)
-    plt.savefig("fig/phi03_Sumita_K_02_sph.pdf")
-    compaction_column(velocity_Sumita_spher, K=.1, **options)
-    plt.savefig("fig/phi03_Sumita_K_01_sph.pdf")
-    compaction_column(velocity_Sumita_spher, K=.05, **options)
-    plt.savefig("fig/phi03_Sumita_K_005_sph.pdf")
 
+    ## top boundary free
+    options["BC"] = "dVdz==0"
+    options["coordinates"] = "cartesian"
+    options["phiN"] = options["phi_init"]
 
+    compaction_column(velocity_Sramek, delta=1., **options)
+    plt.savefig("fig/top_phi03_Sramek_delta_1.pdf")
+    compaction_column(velocity_Sramek, delta=.5, **options)
+    plt.savefig("fig/top_phi03_Sramek_delta_05.pdf")
+    compaction_column(velocity_Sramek, delta=.1, **options)
+    plt.savefig("fig/top_phi03_Sramek_delta_01.pdf")
+
+    compaction_column(velocity_Sumita, K=1., **options)
+    plt.savefig("fig/top_phi03_Sumita_K_1.pdf")
+    compaction_column(velocity_Sumita, K=.5, **options)
+    plt.savefig("fig/top_phi03_Sumita_K_05.pdf")
+
+    options["coordinates"] = "spherical"
+    compaction_column(velocity_Sramek, delta=1., **options)
+    plt.savefig("fig/top_phi03_Sramek_delta_1_sph.pdf")
+    compaction_column(velocity_Sramek, delta=.5, **options)
+    plt.savefig("fig/top_phi03_Sramek_delta_05_sph.pdf")
+    compaction_column(velocity_Sramek, delta=.1, **options)
+    plt.savefig("fig/top_phi03_Sramek_delta_01_sph.pdf")
+
+    compaction_column(velocity_Sumita, K=1., **options)
+    plt.savefig("fig/top_phi03_Sumita_K_1_sph.pdf")
+    compaction_column(velocity_Sumita, K=.5, **options)
+    plt.savefig("fig/top_phi03_Sumita_K_05_sph.pdf")
 
 
 if __name__ == "__main__":
@@ -248,6 +190,6 @@ if __name__ == "__main__":
 
 
     #compaction_column_dVdz()
-    #figures_compaction_only()
+    figures_compaction_only()
 
     plt.show()
