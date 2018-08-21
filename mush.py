72dc2eb9206811428891a9337672610e85f9755f
Marine Lasbleis <marine.lasbleis@gmail.com>
2018-08-21 14:02:07 +0900

    pep8

--- mush.py
+++ mush.py
@@ -1,14 +1,21 @@
-""" Reproduction of calculations from Sumita et 1996 on compaction and implications for Earth's inner core """
+""" Solver for 2-phase flow equations in 1D
+
+To be used to study compaction in the Earth's inner core.
+Boundary conditions are specified to either study
+- close boundary at top
+- open boundary with fluid expelled from the system.
+"""
 
 
 import numpy as np
-import scipy as sc
-import matplotlib.pyplot as plt
+# import scipy as sc
+# import matplotlib.pyplot as plt
 
-from tdma import *
+import tdma
 
 
-def Schema():
+def schema():
+    """ Description of the discretization for numerical scheme"""
     print(" <-1 volume-> \n\
 ||-----.-----|-----.-----|       ...      .-----|-----.-----|-----.        ...       -----.-----|-----.-----||  \n\
        0     0     1     1                                                                N-1  N-1    N          \n\
@@ -23,8 +30,8 @@ def Schema():
 # Nt = 10 #number of points in time
 
 
-def fluxlimiterscheme(velocity, variable, dr, options={}):
-    """ output the coefficients for the advection scheme using the flux limiter scheme.
+def fluxlimiterscheme(velocity, variable, dr, options):
+    """ Advection scheme using the flux limiter scheme
 
     Coefficients for the scheme are lambda+, lambda-, v+ and v-
     (p and m are used instead of + and m)
@@ -41,7 +48,6 @@ def fluxlimiterscheme(velocity, variable, dr, options={}):
     """
 
     # detect size of the system and initialize variables:
-    #lambdap, lambdam, vp, vm = np.zeros(len(variable)+1), np.zeros(len(variable)+1), np.zeros(len(variable)+1), np.zeros(len(variable)+1)
     lambdap, lambdam, vp, vm = np.zeros_like(velocity), np.zeros_like(
         velocity), np.zeros_like(velocity), np.zeros_like(velocity)
     _a, _b, _c, _d = np.zeros_like(variable), np.zeros_like(
@@ -70,10 +76,8 @@ def fluxlimiterscheme(velocity, variable, dr, options={}):
         print("Problem with the choosen scheme for advection. Default is upwind.")
 
     if len(velocity) == 1:
-        if velocity > 0:
-            vp[:] = velocity
-        else:
-            vm[:] = - velocity
+        if velocity > 0: vp[:] = velocity
+        else: vm[:] = - velocity
     else:
         vp[:] = 0.5 * (velocity[:] + np.abs(velocity[:]))
         vm[:] = 0.5 * (velocity[:] - np.abs(velocity[:]))
@@ -87,12 +91,14 @@ def fluxlimiterscheme(velocity, variable, dr, options={}):
 
     # boundary conditions:
     # velocity fixed at U0 and UN
-    # porosity fixed at phi0 and phiN (phi0 and phiN correspond to phi_-1 and phi_N+1, the 2 that are not in the array phi)
+    # porosity fixed at phi0 and phiN
+    # (phi0 and phiN correspond to phi_-1 and phi_N+1,
+    # the 2 that are not in the array phi)
     # default is all 0.
 
     try:
         if options["BC"] == "V==0":
-            U0, UN = 0., 0.  # velocity[0], velocity[-1]
+            U0, UN = 0., 0. # velocity[0], velocity[-1]
         elif options["BC"] == "dVdz==0":
             U0, UN = velocity[0], velocity[-1]
     except KeyError:
@@ -106,11 +112,12 @@ def fluxlimiterscheme(velocity, variable, dr, options={}):
             psi0, psiN = 1-options["phi0"], 1-options["phiN"]
         except KeyError:
             psi0, psiN = 0.5, 0.5
-            print("Be careful, boundary conditions not well specified. You need to specify psi0, psiN or phi0, phiN")
+            print("Be careful, boundary conditions not well specified. \
+                    You need to specify psi0, psiN or phi0, phiN")
 
         # minmod
-    lambdap_up = 0.
-    lambdam_up = 0.
+    # lambdap_up = 0.
+    # lambdam_up = 0.
     lambdam_down = 0.
     lambdap_down = 0.
 
@@ -121,8 +128,7 @@ def fluxlimiterscheme(velocity, variable, dr, options={}):
     _b[0] = vp[0] * (1 - lambdap[0] / 2.) + vm[0] * lambdam[0] / 2. \
         - U0m * (1 - lambdam_down / 2.) - U0p * lambdap_down / 2.
     _c[0] = vm[0] * (1 - lambdam[0] / 2.) + vp[0] * lambdap[0] / 2.
-    _d[0] = (_a[0]*psi0 + _b[0] * variable[0] +
-             _c[0] * variable[1]) + _a[0]*psi0
+    _d[0] = (_a[0]*psi0+ _b[0] * variable[0] + _c[0] * variable[1]) +_a[0]*psi0
 
     _a[-1] = -vp[-1]
     _b[-1] = UNp - vm[-1]
@@ -158,11 +164,11 @@ def CrankNicholson(variable, dr, options):
 
 
 def velocity_Sramek(variable, radius, options, verbose=False):
-    """ Sramek thesis p46, equation 3.22
+    r""" Sramek thesis p46, equation 3.22
     $$ \frac{V}{\delta**2\phi**2} = \frac{d}{dz} [ \frac{(K0+4/3\phi)(1-\phi)}{\phi} \frac{d}{dz}V]-s(1-\phi) $$
 
     Variable: length N
-    Output: velocity is length N-1
+    Output: velocity is length N-1s
     a, b, c, d are length N-1, N-1, N-1 and N-1
     and a, c are injected as length N-2 for calculating the tridiagonal matrix.
     """
@@ -229,14 +235,14 @@ def velocity_Sramek(variable, radius, options, verbose=False):
         _d[-1] = _d[-1] - _c[-1]
     elif options["BC"] == "V==0":
         pass
-
-    new_velocity = inversion_matrice(_a[1:], _b, _c[:-1], _d)
+    new_velocity = tdma.inversion_matrice(_a[1:], _b, _c[:-1], _d)
     return new_velocity
 
 
 def velocity_Sumita(variable, radius, options, verbose=False):
-    """ Solve the velocity of the solid, based on equation 9 in Sumita et al. 1996 
+    """ Solve the velocity of the solid
 
+    based on equation 9 in Sumita et al. 1996
     Cartesian and Spherical coordinates (change in options["coordinates"])
     """
     dr = radius[1] - radius[0]  # assuming no variations of dr
@@ -292,8 +298,8 @@ def velocity_Sumita(variable, radius, options, verbose=False):
         _a = - ((1. / (dr**2.)) * ((1. - variable[0:-1])**2.)
                 * (4. / (3. * variable[0:-1])) * (eta / eta0))
         _b = ((1. - np.sqrt(variable[1:] * variable[0:-1]))**2 / (variable[0:-1] * variable[1:])**(3. / 2.)) * ((K * K0) / grain**2.) \
-            + (1. / dr**2.) * (((1. - variable[0:-1])**2.) * (4. / (3. * variable[0:-1])) * (
-                eta / eta0) + ((1. - variable[1:])**2.) * (4. / (3. * variable[1:])) * (eta / eta0))
+                + (1. / dr**2.) * (((1. - variable[0:-1])**2.) * (4. / (3. * variable[0:-1])) * (
+                    eta / eta0) + ((1. - variable[1:])**2.) * (4. / (3. * variable[1:])) * (eta / eta0))
         _c = - ((1. / (dr**2.)) *
                 ((1. - variable[1:])**2.) * (4. / (3. * variable[1:])) * (eta / eta0))
         _d = - sign * ((1. - np.sqrt(variable[1:] * variable[0:-1])))
@@ -302,9 +308,9 @@ def velocity_Sumita(variable, radius, options, verbose=False):
             * (1 / ((radius[0:-2]) + (dr / 2))**2) * ((radius[0:-2])**2 / dr**2)
         _b = ((K * K0) / grain**2) * ((1. - np.sqrt(variable[1:] * variable[0:-1]))**2 / (variable[0:-1] * variable[1:])**(3. / 2.)) \
             + (((4 * (1 - variable[1:])**2) / (3 * variable[1:])) * (eta / eta0)
-               * (1 / ((radius[1:-1]) + (dr / 2))**2) * (((radius[1:-1])**2 / dr**2))) \
+                * (1 / ((radius[1:-1]) + (dr / 2))**2) * (((radius[1:-1])**2 / dr**2))) \
             + (((4 * (1 - variable[0:-1])**2) / (3 * variable[0:-1])) * (eta / eta0)
-               * (1 / ((radius[0:-2]) + (dr / 2))**2) * (((radius[1:-1])**2 / dr**2)))
+                * (1 / ((radius[0:-2]) + (dr / 2))**2) * (((radius[1:-1])**2 / dr**2)))
         _c = -((4 * (1 - variable[1:])**2) / (3 * variable[1:])) * (eta / eta0) \
             * (1 / ((radius[1:-1]) + (dr / 2))**2) * (((radius[2:])**2 / dr**2))
         _d = - sign * \
@@ -325,7 +331,7 @@ def velocity_Sumita(variable, radius, options, verbose=False):
     _c = np.where(too_large, 0., _c)
     _d = np.where(too_large, 0., _d)
 
-    new_velocity = inversion_matrice(_a[1:], _b, _c[:-1], _d)
+    new_velocity = tdma.inversion_matrice(_a[1:], _b, _c[:-1], _d)
     return new_velocity
 
 
@@ -357,9 +363,9 @@ def update(V, psi, dt, radius, options):
     options is a dictionnary, with a bunch of parameters for everything.
     """
     dr = radius[1]-radius[0]
-    # a_adv, b_adv, c_adv, d_adv = fluxlimiterscheme(V, phi, dr, options)
-    # a_diff, b_diff, c_diff, d_diff = CrankNicholson(phi, dr, options)
-    # _a, _b, _c, _d = a_adv+a_diff, b_adv+b_diff, c_adv+c_diff, d_adv+d_diff
+        # a_adv, b_adv, c_adv, d_adv = fluxlimiterscheme(V, phi, dr, options)
+        # a_diff, b_diff, c_diff, d_diff = CrankNicholson(phi, dr, options)
+        # _a, _b, _c, _d = a_adv+a_diff, b_adv+b_diff, c_adv+c_diff, d_adv+d_diff
     _a, _b, _c, _d = fluxlimiterscheme(V, psi, dr, options)
 
     if options["coordinates"] == "spherical":
@@ -371,7 +377,7 @@ def update(V, psi, dt, radius, options):
     _c = _c * dt
     _d = psi - _d * dt
     psi2 = np.zeros_like(psi)
-    _psi = inversion_matrice(_a[1:], _b, _c[:-1], _d)
+    _psi = tdma.inversion_matrice(_a[1:], _b, _c[:-1], _d)
     psi2[:] = _psi
     return psi2
 
