2fac24372da9a4ef553bd03e57ccda6ed9d31cbe
Marine Lasbleis <marine.lasbleis@gmail.com>
2018-09-07 16:29:28 +0200

    work OK for Qcmb > 1e17

--- supercooling.py
+++ supercooling.py
@@ -18,6 +18,7 @@ class Evolution:
 
     def __init__(self, delta_T, Qcmb):
         self.Qcmb = Qcmb
+        print(self.Qcmb)
         self.delta_T = delta_T
         # parameters
         self.longueur = 6500e3 # Lp = sqrt(3Cp/2pialpharhoG)
@@ -32,10 +33,12 @@ class Evolution:
         self.gamma = 1.5 # Gruneisen parameter
         self.eta_c = 0.79 # parameters (see after eq. A.3 in Huguet et al 2018)
         self.Mc = 1.95e24
-        self.Eg = 3e5
+        self.Eg =  3e5
         # numerical solver
+        time_max = 1e6
+        N_time = 5e5
         year = 365.25*3600*24
-        self.time = np.linspace(0, 500*year, 1e3)
+        self.time = np.linspace(0, time_max*year, N_time)
         self.time_years = self.time/year
         self.dt = self.time[1] - self.time[0] # 1ks
         self.dric = np.zeros_like(self.time)
@@ -56,36 +59,36 @@ class Evolution:
     def run_constant_temperature(self):
         self.reinitilization()
         T_center = self.T_Fe(0., 0.) - self.delta_T # initial temperature at center. 
-        self.dric[0] = self.G*(self.delta_T)**2
+        self.dric[0] = 0. #self.G*(self.delta_T)**2
         self.Tic[0] = T_center
-        self.supercooling[0] = self.delta_T
-        self.Tis_center[0] = T_center
+        self.supercooling = self.delta_T
+        self.Tis_center[:] = T_center
         self.Tcmb[0] = self.T_is(self.rc, T_center)
 
-        for i, t in enumerate(self.time[1:]):
-            dric, Tic = self.icoregrowth(T_center, self.ric[i-1], 0.)
-            self.dric[i] = dric
-            self.Tic[i] = Tic
-            self.ric[i] = self.ric[i-1] + dric*self.dt
-            self.supercooling[i] = np.abs(self.T_Fe(self.ric[i]) - Tic)
+        for i, t in enumerate(self.time[:-1]):
+            dric, Tic = self.icoregrowth(T_center, self.ric[i], 0.)
+            self.dric[i+1] = dric
+            self.Tic[i+1] = Tic
+            self.ric[i+1] = self.ric[i] + dric*self.dt
+            #self.supercooling[i] = np.abs(self.T_Fe(self.ric[i]) - Tic)
 
     def run(self):
         self.reinitilization()
-        T_center = self.T_Fe(0., 0.) - self.delta_T # initial temperature at center. 
-        self.dric[0] = self.G*(self.delta_T)**2
+        T_center = self.T_Fe(0., 0.) - self.delta_T # initial temperature at center.
+        self.dric[0] = 0. #self.G*(self.delta_T)**2
         self.Tic[0] = T_center
-        self.supercooling[0] = self.delta_T
+        self.supercooling = self.delta_T
         self.Tis_center[0] = T_center
         self.Tcmb[0] = self.T_is(self.rc, T_center)
-
         for i, t in enumerate(self.time[1:]):
-            dric, Tic = self.icoregrowth(self.Tis_center[i-1], self.ric[i-1], 0.)
-            self.dric[i] = dric
-            self.Tic[i] = Tic
-            self.ric[i] = self.ric[i-1] + dric*self.dt
-            self.supercooling[i] = np.abs(self.T_Fe(self.ric[i]) - Tic)
-            self.Tis_center[i] = self.Tis_center[i-1] + self.dTcmb(dric, self.ric[i])*self.dt
-            self.Tcmb[i] = self.T_is(self.ric[i], self.Tis_center[i])
+            dric, Tic = self.icoregrowth(self.Tis_center[i], self.ric[i], 0.)
+            self.dric[i+1] = dric
+            self.Tic[i+1] = Tic
+            self.ric[i+1] = self.ric[i] + dric*self.dt
+            #self.supercooling[i] = np.abs(self.T_Fe(self.ric[i]) - Tic)
+            self.Tis_center[i+1] = self.Tis_center[i] + self.dTcmb(dric, self.ric[i+1])*self.dt
+            self.Tcmb[i+1] = self.T_is(self.ric[i+1], self.Tis_center[i+1])
+            if i%1e4==0: print(i)
 
     def T_is(self, radius, T0):
         # T0 is temperature at center
@@ -105,7 +108,7 @@ class Evolution:
             y0 = rd - self.G*(Tm - Ti)**2
             y1 = self.rho0*(self.cp*(Tm - Ta) + self.LFe)*rd - self.rhol*self.cp*self.u*(Ti - Ta)
             return np.array([y0, y1])
-        a, _, ierr, msg = fsolve(func, np.array([1e-9, T]), xtol=1e-5, args=(T, ric, cc,), full_output=True)
+        a, _, ierr, msg = fsolve(func, np.array([1e-9, T]), xtol=1e-4, args=(T, ric, cc,), full_output=True)
         if ierr != 1:
             print(ierr, a, msg)
         return a[0], a[1]
@@ -119,27 +122,35 @@ class Evolution:
         return self.eta_c/self.Mc/self.cp * (self.QLQG(dricdt, ric) - self.Qcmb)
 
     def plot(self):
-        fig, ax = plt.subplots(1,2, sharey=True)
+        fig, ax = plt.subplots(1,3, sharey=True)
         r = np.linspace(0, 1221e3, 30)
         ax[0].plot(self.T_is(r, 6400), r, 'r', label="T isentrope_beg")
         #ax[0].plot(self.T_is(r, self.Tis_center[-1]), r, 'r', label="T isentrope_end")
         #ax[0].plot(self.T_is(r, self.Tis_center[500]), r, 'r', label="T isentrope_middle")
         ax[0].plot(self.T_Fe(r), r, 'b', label="melting T of iron")
         ax[0].plot(self.Tic[:-1], self.ric[:-1], 'k', label="T_ic")
-        #ax[0].plot(self.Tis_center[:-1], self.ric[:-1], '.', label="Tcenter")
+        ax[0].plot(self.Tis_center[:-1], self.ric[:-1], 'g', label="Tcenter")
+        ax[2].plot(np.diff(self.Tcmb)[1:], self.ric[1:-1], 'g', label="dot r")
         ax[0].legend()
         ax[1].plot(self.time_years[:-1], self.ric[:-1])
+        ax[0].set_ylabel("Radius")
+        ax[0].set_xlabel("Temperature")
+        ax[1].set_xlabel("Time")
         print(self.ric[-2])
+        print(np.diff(self.Tcmb)[-3:])
 
 
 if __name__ == "__main__":
 
     delta_T = 100 #initial supercooling
 
-    test = Evolution(delta_T, 1e-12)
+    test = Evolution(delta_T, 1e17)
     test.run()
     test.plot()
-    test = Evolution(delta_T, 1e-12)
-    test.run_constant_temperature()
-    test.plot()
+
+    #test = Evolution(delta_T, 1e9)
+    #test.run()
+    #test.plot()
+    #test.run_constant_temperature()
+    #test.plot()
     plt.show()
