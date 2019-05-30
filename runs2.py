60dacad12a10613da3a7c1a5394ce61f941d1628
Marine Lasbleis <marine.lasbleis@gmail.com>
2019-05-30 09:40:46 -0600

    update runs

--- runs2.py
+++ runs2.py
@@ -149,9 +149,9 @@ def param_supercooling(r, exp, coeff, r0_supercooling, n=2, N_fig=20, basefolder
 
 def run_no_growth(n=3):
     #Ric = 10**(np.linspace(-1, 2, 3))
-    Ric = np.array([100.])
+    Ric = np.array([10.])
     for radius in Ric:
-        output = "no_growth_n3/output_{:.2e}".format(radius)
+        output = "output_cartesian_{:.2e}".format(radius)
         options = param_no_growth(radius.item(), 1e3, 10, N=5000, output=output, n=n)
         run(options)
 
@@ -231,4 +231,4 @@ if __name__ == "__main__":
     #if args.verbose:
     #    print("verbosity turned on")
 
-    run_growth_random()
+    run_no_growth()
